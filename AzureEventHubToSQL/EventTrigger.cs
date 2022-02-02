b57d4b64072d0a707b1b3a32d9e5ee4d3b1e73ff
obinna <oigbokwe73@gmail.com>
2022-02-01 21:45:31 -0500

    Add project files.

---
+++ AzureEventHubToSQL/EventTrigger.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Xenhey.BPM.Core;
+using Xenhey.BPM.Core.Implementation;
+
+namespace AzureEventHubToSQL
+{
+    public class EventHubToSQL
+    {
+        private NameValueCollection nvc = new NameValueCollection();
+
+        [FunctionName("EventHubToSQL")]
+        public async Task Run([EventHubTrigger("training202201", Connection = "EventHubConnectionAppSetting")] EventData[] events, ILogger log)
+        {
+
+            var exceptions = new List<Exception>();
+
+            foreach (EventData eventData in events)
+            {
+                try
+                {
+                    string messageBody = Encoding.UTF8.GetString(eventData.Body);
+                    byte[] byteArray = Encoding.ASCII.GetBytes(messageBody);
+                    var streamData = new MemoryStream(byteArray);
+                    var results = orchrestatorService.Run(streamData);
+                    log.LogInformation($"EnqueuedTimeUtc={eventData.SystemProperties.EnqueuedTimeUtc}");
+                    await Task.Yield();
+                }
+                catch (Exception e)
+                {
+                    // We need to keep processing the rest of the batch - capture this exception and continue.
+                    // Also, consider capturing details of the message that failed processing so it can be processed again later.
+                    exceptions.Add(e);
+                }
+            }
+
+            // Once processing of the batch is complete, if any messages in the batch failed processing throw an exception so that there is a record of the failure.
+
+            if (exceptions.Count > 1)
+                throw new AggregateException(exceptions);
+
+            if (exceptions.Count == 1)
+                throw exceptions.Single();
+        }
+        private IOrchrestatorService orchrestatorService
+        {
+            get
+            {
+                nvc.Add("x-api-key", "0F1AE83D158143AC84F6150F62B29712");
+                return new ManagedOrchestratorService(nvc);
+            }
+        }
+    }
+}
---
+++ AzureEventHubToSQL/uploadfile.cs
@@ -0,0 +1,52 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Xenhey.BPM.Core.Implementation;
+using Xenhey.BPM.Core;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace AzureEventHubToNoSQLDB
+{
+    public class UploadFile
+    {
+        private HttpRequest _req;
+        private NameValueCollection nvc = new NameValueCollection();
+        [FunctionName("uploadfile")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+            HttpRequest req, ILogger log)
+        {
+            _req = req;
+
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            var results = orchrestatorService.Run(_req.Body);
+            return resultSet(results);
+
+        }
+
+        private ActionResult resultSet(string reponsePayload)
+        {
+            var returnContent = new ContentResult();
+            var mediaSelectedtype = _req.Headers.Where(x => x.Key.Equals("Content-Type")).FirstOrDefault();
+            returnContent.Content = reponsePayload;
+            returnContent.ContentType = mediaSelectedtype.Value;
+            return returnContent;
+        }
+        private IOrchrestatorService orchrestatorService
+        {
+            get
+            {
+
+                _req.Headers.ToList().ForEach(item =>
+                {
+                    nvc.Add(item.Key, item.Value.FirstOrDefault());
+                });
+                return new ManagedOrchestratorService(nvc);
+            }
+        }
+
+    }
+}
