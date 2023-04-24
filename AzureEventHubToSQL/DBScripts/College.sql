0d1816014106afcd1aa33a7da45a9383356cbd17
obinna <oigbokwe73@gmail.com>
2023-04-24 08:36:38 -0400

    updates to include connectivity to SQL Database

---
+++ AzureEventHubToSQL/DBScripts/College.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[College] (
+    [Company]   NVARCHAR (MAX) NULL,
+    [Address]    NVARCHAR (MAX) NULL,
+    [City]       NVARCHAR (MAX) NULL,
+    [State]    NVARCHAR (MAX) NULL,
+    [Zip]       NVARCHAR (MAX) NULL,
+    [County]      NVARCHAR (MAX) NULL,
+    [Phone]        NVARCHAR (MAX) NULL,
+    [Website]       NVARCHAR (MAX) NULL,
+    [Contact]  NVARCHAR (MAX) NULL,
+    [Title]       NVARCHAR (MAX) NULL,
+    [Direct Phone]     NVARCHAR (MAX) NULL,
+    [Email] NVARCHAR (MAX) NULL,
+    [Sales]     NVARCHAR (MAX) NULL,
+    [Employees] NVARCHAR (MAX) NULL,
+    [SIC Code]    NVARCHAR (MAX) NULL,
+    [Industry]   NVARCHAR (MAX) NULL
+);
+
---
+++ AzureEventHubToSQL/DBScripts/Football.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Football]
+    (
+        [data]            nvarchar(max) NULL,
+        [team]            nvarchar(max) NULL,
+        [time]            nvarchar(max) NULL,
+        [Opponent]        nvarchar(max) NULL,
+        [Rank]            nvarchar(max) NULL,
+        [Site]            nvarchar(max) NULL,
+        [Result]          nvarchar(max) NULL,
+        [Attendance]      nvarchar(max) NULL,
+        [CurrentWins]     nvarchar(max) NULL,
+        [CurrentLosses]   nvarchar(max) NULL,
+        [StadiumCapacity] nvarchar(max) NULL,
+        [FillRate]        nvarchar(max) NULL,
+        [NewCoach]        nvarchar(max) NULL,
+        [Tailgating]      nvarchar(max) NULL
+    );
